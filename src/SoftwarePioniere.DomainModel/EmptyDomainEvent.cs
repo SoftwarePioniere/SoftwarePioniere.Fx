49f43004ed0927d13e20ddfeb1b51b6ff632737e
Tobias Böker <tb@softwarepioniere.de>
2018-07-02 14:25:48 +0200

    added empty domain event added testconfiguration

---
+++ src/SoftwarePioniere.DomainModel/EmptyDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SoftwarePioniere.Messaging;
+
+namespace SoftwarePioniere.DomainModel
+{
+    public class EmptyDomainEvent : DomainEventBase
+    {
+        public EmptyDomainEvent() : base(Guid.NewGuid(), DateTime.MinValue, "userId", "aggregateId")
+        {
+
+        }
+
+    }
+}
---
+++ src/SoftwarePioniere.TestHarness/TestConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace SoftwarePioniere
+{
+
+    public class TestConfiguration
+    {
+        public TestConfiguration()
+        {
+            Console.WriteLine($"AppContext.BaseDirectory: {AppContext.BaseDirectory}");
+            var basePath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..");
+            Console.WriteLine($"Base Path: {basePath}");
+            var fullPath = Path.GetFullPath(basePath);
+            Console.WriteLine($"FullBasePath: {fullPath}");
+
+            // ReSharper disable once UnusedVariable
+            var builder = new ConfigurationBuilder()
+                //.AddEnvironmentVariables()
+                .SetBasePath(fullPath)
+                .AddJsonFile("appsettings.secret.json", true, true)
+                .AddJsonFile("appsettings.json", true, true);
+
+#if !DEBUG
+           builder.AddEnvironmentVariables("SOPI_TESTS_");
+#endif
+
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
+        public IConfiguration ConfigurationRoot { get; private set; }
+
+        public T Get<T>(string sectionName)
+        {
+            return ConfigurationRoot.GetSection(sectionName).Get<T>();
+        }
+
+    }
+}
