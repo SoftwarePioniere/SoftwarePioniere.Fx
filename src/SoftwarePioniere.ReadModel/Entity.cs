22f3b13f48d33cc2b603f1e63eed7c12f10fc9dd
Tobias Böker <tb@softwarepioniere.de>
2018-10-30 13:42:24 +0100

    readmodel to string override

--- src/SoftwarePioniere.ReadModel/Entity.cs
+++ src/SoftwarePioniere.ReadModel/Entity.cs
@@ -34,5 +34,13 @@ namespace SoftwarePioniere.ReadModel
         [JsonProperty("modified_on_utc")]
         [Required]
         public DateTime ModifiedOnUtc { get; set; }
+
+        public override string ToString()
+        {
+            if (!string.IsNullOrEmpty(EntityId))
+                return EntityId;
+
+            return base.ToString();
+        }
     }
 }
