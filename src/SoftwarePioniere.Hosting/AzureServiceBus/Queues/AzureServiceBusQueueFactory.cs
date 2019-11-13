11deb0915b3249043fab4bd4c16a4b5674f52c9d
Tobias Böker <tb@softwarepioniere.de>
2019-11-13 15:14:57 +0100

    azure que factory options

--- src/SoftwarePioniere.Hosting/AzureServiceBus/Queues/AzureServiceBusQueueFactory.cs
+++ src/SoftwarePioniere.Hosting/AzureServiceBus/Queues/AzureServiceBusQueueFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Foundatio.Messaging;
 using Foundatio.Queues;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SoftwarePioniere.Messaging;
@@ -10,15 +11,19 @@ namespace SoftwarePioniere.Hosting.AzureServiceBus.Queues
     public class AzureServiceBusQueueFactory : IQueueFactory
     {
         private readonly ILoggerFactory _loggerFactory;
-        private readonly IOptions<AzureServiceBusMessageBusOptions> _options;
+        private readonly AzureServiceBusMessageBusOptions _messageBusOptions;
+        private readonly IServiceProvider _serviceProvider;
 
         public AzureServiceBusQueueFactory(ILoggerFactory loggerFactory,
-            IOptions<AzureServiceBusMessageBusOptions> options
+            IOptions<AzureServiceBusMessageBusOptions> options,
+            IServiceProvider serviceProvider
 
         )
         {
+            if (options == null) throw new ArgumentNullException(nameof(options));
             _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
-            _options = options ?? throw new ArgumentNullException(nameof(options));
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            _messageBusOptions = options.Value;
         }
         public IQueue<T> CreateQueue<T>(string name) where T : class
         {
@@ -26,14 +31,17 @@ namespace SoftwarePioniere.Hosting.AzureServiceBus.Queues
 
             logger.LogInformation("Create new AzureServiceBus Queue {Name} for {Type}", name, typeof(T).Name);
 
-            var opt = _options.Value;
+            var iopt = _serviceProvider.GetService<IOptions<AzureServiceBusQueueOptions<T>>>();
 
-            var queue = new AzureServiceBusQueue<T>(new AzureServiceBusQueueOptions<T>()
-            {
-                ConnectionString = opt.ConnectionString,
-                Name = name,
-                LoggerFactory = _loggerFactory
-            });
+            if (iopt == null)
+                throw new InvalidOperationException($"Please Register AzureServiceBusQueueOptions {typeof(T).FullName}");
+
+            var options = iopt.Value;
+            options.ConnectionString = _messageBusOptions.ConnectionString;
+            options.Name = name;
+            options.LoggerFactory = _loggerFactory;
+
+            var queue = new AzureServiceBusQueue<T>(options);
 
             return queue;
         }
